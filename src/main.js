54cbe8188a08c3782b6cd7217f6c52b4762aed89
Mr.yang <oucyxw@qq.com>
2022-02-22 16:21:16 +0800

    优化了echarts的导入

--- src/main.js
+++ src/main.js
@@ -14,13 +14,24 @@ import  VueQuillEditor from 'vue-quill-editor'
 import 'quill/dist/quill.core.css'
 import 'quill/dist/quill.snow.css'
 import 'quill/dist/quill.bubble.css'
+import NProgress from 'nprogress'
+import 'nprogress/nprogress.css'
+
 Vue.use(VueQuillEditor)
 
 
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+//在 request拦截器中， 显示进度条 NProgress.start()
 axios.interceptors.request.use(config => {
+  NProgress.start()
   config.headers.Authorization = window.sessionStorage.getItem('token')
   console.log(config)
+  //必须返回config
+  return config
+})
+//在 response 拦截器中，隐藏进度条 NProgress.done()
+axios.interceptors.response.use(config => {
+  NProgress.done()
   return config
 })
 Vue.prototype.$http = axios
